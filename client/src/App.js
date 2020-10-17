4bcad9016ef285b179caa1642d1c9a809738aa5f
samarth30 <samarth.gugnani30@gmail.com>
2020-10-18 03:10:44 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,8 @@ import Web3 from "web3";
 import Navbar from "./Navbar";
 
 const App = () => {
-  useEffect(() => {
-    loadWeb3();
-    loadBlockchainData();
+  const [refresh, setrefresh] = useState(0);
 
-    //esl
-  }, []);
   let content;
   const [loading2, setloading2] = useState(false);
 
@@ -32,6 +28,12 @@ const App = () => {
 
   const loadBlockchainData = async () => {
     setLoading(true);
+    if (
+      typeof window.ethereum == "undefined" ||
+      typeof window.web3 == "undefined"
+    ) {
+      return;
+    }
     const web3 = window.web3;
 
     let url = window.location.href;
@@ -39,9 +41,12 @@ const App = () => {
 
     const accounts = await web3.eth.getAccounts();
 
+    if (accounts.length == 0) {
+      return;
+    }
     setAccount(accounts[0]);
     const networkId = await web3.eth.net.getId();
-    const networkData = Helloabi.networks[networkId];
+    const networkData = Helloabi.abi.networks[networkId];
     console.log(networkData);
     if (networkData) {
       const hello = new web3.eth.Contract(Helloabi.abi, networkData.address);
@@ -49,21 +54,75 @@ const App = () => {
 
       setLoading(false);
     } else {
-      window.alert("Forsage contract not deployed to detected network.");
+      window.alert("the contract not deployed to detected network.");
       setloading2(true);
     }
   };
 
+  const onclick = async (a) => {
+    await Hello.methods
+      .setCompleted(a.toString())
+      .send({ from: account })
+      .once("recepient", (recepient) => {
+        console.log("success");
+      })
+      .on("error", () => {
+        console.log("error");
+      });
+  };
+
+  const walletAddress = async () => {
+    await window.ethereum.request({
+      method: "eth_requestAccounts",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+
+    if (refresh == 1) {
+      setrefresh(0);
+      loadBlockchainData();
+    }
+    //esl
+  }, [refresh]);
+
   if (loading === true) {
     content = (
       <p className="text-center">
-        Loading...{loading2 ? <div>load on mainenet </div> : ""}
+        Loading...{loading2 ? <div>loading....</div> : ""}
       </p>
     );
   } else {
     content = (
       <div class="container">
-        <h1>hello</h1>
+        <main role="main" class="container">
+          <div class="jumbotron">
+            <h1>Project</h1>
+            <div className="row" style={{ paddingTop: "30px" }}>
+              {" "}
+              <div className="row" style={{ paddingLeft: "40px" }}>
+                <h3>text 1</h3>
+              </div>
+              <div className="row" style={{ paddingLeft: "40px" }}>
+                <h3>text 2</h3>
+              </div>
+              <div className="row" style={{ paddingLeft: "40px" }}>
+                <h3>text 3</h3>
+              </div>
+              <div className="row" style={{ paddingLeft: "40px" }}>
+                <button className="btn btn-primary">Click on it</button>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
@@ -71,7 +130,17 @@ const App = () => {
   return (
     <div>
       <Navbar account={account} />
-      {content}
+
+      {account == "" ? (
+        <div className="container">
+          {" "}
+          Connect your wallet to application{"   "}{" "}
+          <button onClick={walletAddress}>metamask</button>
+        </div>
+      ) : (
+        content
+      )}
+      {/* {content} */}
     </div>
   );
 };
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Navbar = (props) => {
+const Navbar = ({ account }) => {
   return (
     <nav className="navbar navbar-dark bg-dark shadow mb-5">
       <p className="navbar-brand my-auto">react website</p>
       <ul className="navbar-nav">
-        <li className="nav-item text-white">{props.account}</li>
+        <li className="nav-item text-white">{account}</li>
       </ul>
     </nav>
   );
